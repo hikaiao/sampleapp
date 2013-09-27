cbbed146afe2db76f0a0881298d6c6234405ea4a
Hika Iao <hika1990@gmail.com>
2013-09-27 13:46:57 +0000

    First Start in Nitrous

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+SampleApp::Application.config.secret_key_base = '350c4ac3b7a24470ef872720cb7aed3bc3f4b35e6c9f31240bab161eabe5a2bb5509d35531062662adff2bdf71f159c47208d7657f05eeb430e81efe64e7fdc7'
